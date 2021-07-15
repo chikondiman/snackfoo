f0f7a4cc08ec477f1ccec19becb8ee4bb1eb063c
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-07-15 08:11:20 -0500

    Update Poll.js

--- src/Poll.js
+++ src/Poll.js
@@ -24,10 +24,10 @@ export default class Poll extends React.Component
 
   render() {
    return 
-
+(
 
  <button onClick={this.castVote} className="button">{this.state.snack}</button> 
 
-   
+)
 
-};
+}
