06d9d12bc58d4273a0938a6db1ab4c879d401e7c
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-07-28 16:02:59 -0400

    Update Poll.js

--- src/Poll.js
+++ src/Poll.js
@@ -1,36 +1,62 @@
-import React from 'react';
+
+
+import React, { useState } from 'react';
+
+function Component() {
+    const [snack1, setCount1] = useState(0);
+    const [snack2, setCount2] = useState(0);
+
+    return (
+     <div>
+
+        <p>You clicked {snack1} times</p>
+       <button onClick={(snack1) => setCount1(snack1 + 1)}>
+        Click me
+        </button>
+
+        <button className="button">General Mills Chex Mix</button>
+
+<button className="button">Clif Chocolate Chip Energy Bar</button>
+
+<button className="button">Cheetos Chips</button>
+
+<button className="button">Baked Lays Potato Chips</button>
+
+<button className="button">Hersheys Milk Chocolate</button>
+
+<button className="button">Jack Links Beef Jerky</button>
+
+<button className="button">Planters Salted Peanuts</button> 
+</div>
+
+</td>
+<table>
 
 
 
+  <tr>
+    <th>Selection</th>
+    <th> Vote </th>
+ 
+  </tr>
+  <tr>
+    <td>Nabisco Oreo Cookies</td>
+    <td> {count}</td>
 
-export default class Poll extends React.Component
+  </tr>
+  <tr>
+    <td>General Mills Chex Mix</td>
+    <td>0</td>
+  </tr>
+</table>
+</table>
 
-{
+</div>
+      </div>
+   );
+  }
+
   
-  constructor(props) {
-    super(props)
-    this.state = {
-      vote: 3,
-      count: 3,
-      snack: "Nabisco Oreo Cookies"
-    }
-  
-
-  
-
-    function castVote() {
-      this.state.vote = this.state.vote + 1
-    }
-
-};
+  export default Component 
 
 
-  render() {
-   return 
-(
-
- <button onClick={this.castVote} className="button">{this.state.snack}</button> 
-
-);
-
-  }}
