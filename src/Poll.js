53b18ac0f8adecc48038198f20b2ece061bb4134
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-07-29 13:25:17 -0400

    Update Poll.js

--- src/Poll.js
+++ src/Poll.js
@@ -14,44 +14,7 @@ function Component() {
         Click me
         </button>
 
-        <button className="button">General Mills Chex Mix</button>
-
-<button className="button">Clif Chocolate Chip Energy Bar</button>
-
-<button className="button">Cheetos Chips</button>
-
-<button className="button">Baked Lays Potato Chips</button>
-
-<button className="button">Hersheys Milk Chocolate</button>
-
-<button className="button">Jack Links Beef Jerky</button>
-
-<button className="button">Planters Salted Peanuts</button> 
-</div>
-
-</td>
-<table>
-
-
-
-  <tr>
-    <th>Selection</th>
-    <th> Vote </th>
- 
-  </tr>
-  <tr>
-    <td>Nabisco Oreo Cookies</td>
-    <td> {count}</td>
-
-  </tr>
-  <tr>
-    <td>General Mills Chex Mix</td>
-    <td>0</td>
-  </tr>
-</table>
-</table>
-
-</div>
+        
       </div>
    );
   }
